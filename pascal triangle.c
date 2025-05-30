fa80b211770f97a9426b6c56e5a03b24faa758a6
eureca114 <107104321+eureca114@users.noreply.github.com>
2025-05-30 21:40:36 +0530

    Update pascal triangle.c

--- pascal triangle.c
+++ pascal triangle.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
-#include<conio.h>
-main()
+int main()
 {
   int x,i,j;
   printf("enter the height of Triangle-");
   scanf("%d",&x);
-  int p[x]={1},c[x];
-  p[-1]=0;
+  int p[x],c[x];
+  p[-1]=0,p[0]=1;
   for(i=0;i<x;i++)
     {
       for(j=0;j<i;j++)
@@ -16,6 +15,7 @@ main()
         }
       printf("\n");
       for(j=0;j<i;j++) p[j]=c[j];
-      c[j]=0;
+      p[j+1]=0;
     }
+  return 0
 }
