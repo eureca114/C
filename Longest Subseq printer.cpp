557b99d20f512f68ec0070943620d1b93707c707
eureca114 <107104321+eureca114@users.noreply.github.com>
2025-06-08 08:42:18 +0530

    Add files via upload

---
+++ Longest Subseq printer.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int x,count[]={1,0},max[]={0,0};
+	printf("Enter the number of numbers you wanna enter-");
+	scanf("%d",&x);
+	int a[x];
+	for(int i=0;i<x;i++)
+	{
+		printf("enter %d number-",i+1);
+		scanf("%d",&a[i]);
+		if(i>0)
+		{
+			if(a[i]>a[i-1]) count[0]++;
+			else
+			{
+				if(max[0]<count[0]) max[0]=count[0],max[1]=count[1];
+				count[0]=1,count[1]=i;
+			}
+		}
+	}
+	if(max[0]<count[0]) max[0]=count[0],max[1]=count[1];
+	for(int i=max[1];i<(max[1]+max[0]);i++) printf("%d\n",a[i]);
+}
